2f63a8f19b12141e7c09b96ce146a789857a3622
Jesus <jesusgil1993@outlook.com>
2023-08-23 02:49:53 -0700

    Create existing LoginAndRegistrationDemoApp project

---
+++ app/src/main/java/io/github/loginandregistrationdemoapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package io.github.loginandregistrationdemoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val c1 = Color(0xffccecfc)
+val c2 = Color(0xff91d1fc)
+val c3 = Color(0xffcbc0fc)
+val c4 = Color(0xffe8bdfc)
+val c5_ = Color(0xffcbd8fc)
+val c6 = Color(0xffa9befc)
+val c7 = Color(0xffcda8fc)
+val c8 = Color(0xffb4c1fc)
+val c9 = Color(0xffc49bfc)
